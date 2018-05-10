9cae2b902352a3d6596d2262ada3821f89a39976
AaronWang <yuxianzhiyanai@163.com>
2018-05-11 00:15:07 +0800

    Update test-pages.js

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,6 +1,6 @@
 var expect  = require('chai').expect;
 var request = require('request');
 var server = process.env['SERVER']||'localhost';
-it('Main page content', function(done) {
+it('Test Success', function(done) {
     done();
 });
