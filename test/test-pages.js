455a368313ffb155ca1dc58c092b5c70c2325f9a
AaronWang <yuxianzhiyanai@163.com>
2018-05-10 23:35:50 +0800

    Update test-pages.js

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,6 +1,6 @@
 var expect  = require('chai').expect;
 var request = require('request');
-var server = process.env['SERVER']||localhost;
+var server = process.env['SERVER']||'localhost';
 it('Main page content', function(done) {
     request('http://' + server + ':3000' , function(error, response, body) {
       expect(body).to.equal('Hello World');
